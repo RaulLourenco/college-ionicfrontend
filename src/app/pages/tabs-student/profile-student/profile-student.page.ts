2397b1d584356c8e9caa208193fd09187159cf07
Raul Lourenço <raulourenco@hotmail.com>
2019-11-25 23:06:46 -0300

    creating pages to student profile

--- src/app/pages/tabs-student/profile-student/profile-student.page.ts
+++ src/app/pages/tabs-student/profile-student/profile-student.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-profile-student',
   templateUrl: './profile-student.page.html',
@@ -7,9 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileStudentPage implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+
+  public exit() {
+    this.router.navigate(['/login']);
   }
 
 }
